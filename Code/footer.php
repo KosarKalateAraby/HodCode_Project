3d929ab27a53f4070ab304384abc45b52d4d19ac
KosarKalateAraby <hamedmrym@gamil.com>
2025-01-29 14:28:49 +0330

    Update functions

--- Code/footer.php
+++ Code/footer.php
@@ -4,40 +4,40 @@
             <!-- لینک‌های مفید -->
             <div class="col-md-3 text-md-right text-left">
                 <h3 class="font-weight-bold">لینک‌های مفید</h3>
-                <p><a href="#" class="text-white">صفحه اصلی</a></p>
-                <p><a href="#" class="text-white">محصولات</a></p>
-                <p><a href="#" class="text-white">وبلاگ</a></p>
+                <p><a href="<?php echo home_url()?>" class="text-white">صفحه اصلی</a></p>
+                <p><a href="<?php echo site_url('/shop')?>" class="text-white">محصولات</a></p>
+                <p><a href="<?php echo site_url('/blogs')?>" class="text-white">بلاگ</a></p>
             </div>
 
             <!-- اطلاعات تماس -->
             <div class="col-md-4 text-md-right text-left">
                 <h3 class="font-weight-bold">اطلاعات تماس</h3>
-                <div>
-                    <!-- شماره تماس با آیکون‌ها -->
-                    <div class="d-flex align-items-center mb-2">
-                        <p class="mb-0">9370000000+</p>
-                        <div>
-                            <a href="https://telegram.org" target="_blank" class="text-white mx-2">
-                                <i class="fab fa-telegram" style="font-size: 24px;"></i>
-                            </a>
-                            <a href="https://instagram.com" target="_blank" class="text-white mx-2">
-                                <i class="fab fa-instagram" style="font-size: 24px;"></i>
-                            </a>
-                            <a href="https://linkedin.com" target="_blank" class="text-white mx-2">
-                                <i class="fab fa-linkedin" style="font-size: 24px;"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- ایمیل و آدرس -->
-                    <p class="mb-0">DibajShop@gmail.com</p>
+                <div class="contact-info">
+                    <!-- شماره تماس -->
+                    <p class="mb-2">9370000000+</p>
+                    <!-- ایمیل -->
+                    <p class="mb-2">DibajShop@gmail.com</p>
+                    <!-- آدرس -->
                     <p>ایران - مشهد</p>
                 </div>
             </div>
 
-            <!-- لوگوی eNAMAD -->
-            <div class="col-md-5 d-flex justify-content-end align-items-center">
+            <!-- لوگوی eNAMAD و آیکون‌ها -->
+            <div class="col-md-5 d-flex justify-content-end align-items-center flex-column-reverse flex-md-row">
+                <div class="social-icons mb-3 mb-md-0 ml-md-3">
+                    <a href="https://telegram.org" target="_blank" class="text-white mx-2">
+                        <i class="fab fa-telegram" style="font-size: 24px;"></i>
+                    </a>
+                    <a href="https://instagram.com" target="_blank" class="text-white mx-2">
+                        <i class="fab fa-instagram" style="font-size: 24px;"></i>
+                    </a>
+                    <a href="https://linkedin.com" target="_blank" class="text-white mx-2">
+                        <i class="fab fa-linkedin" style="font-size: 24px;"></i>
+                    </a>
+                </div>
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/image/enamad.jpg" alt="eNAMAD Logo" class="img-fluid" style="width: 100px; height: 100px;">
             </div>
+
         </div>
 
         <div class="row mt-3 justify-content-between">
